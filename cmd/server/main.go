93dae48cba69c96f4d01782353ac0bb6a3f2d860
James Kairos <jamesdkairos@gmail.com>
2026-04-24 15:41:57 -0500

    feat: add pubsub.go & add pause state

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,23 +6,35 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	const connectionString = "amqp://guest:guest@localhost:5672/"
+	const exchange = routing.ExchangePerilDirect
+	const pauseKey = routing.PauseKey
 	fmt.Println("Starting Peril server...")
 
-	connection, err := amqp.Dial(connectionString)
+	conn, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("could not connect to RabbitMQ: %v", err)
 	}
 	defer func() {
-		if err := connection.Close(); err != nil {
+		if err := conn.Close(); err != nil {
 			log.Printf("error closing connection to RabbitMQ: %v", err)
 		}
 	}()
 	fmt.Println("Connection to RabbitMQ successful")
+
+	pauseChann, err := conn.Channel()
+	if err != nil {
+		log.Printf("error opening pause/resume channel: %v", err)
+	}
+	if err = pubsub.PublishJSON(pauseChann, exchange, pauseKey, routing.PlayingState{IsPaused: true}); err != nil {
+		log.Printf("error with pause channel: %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
